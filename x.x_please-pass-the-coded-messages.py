b11c0b05873c3fc1edd23eb7d8ee045aa25da503
velcorn <iwishyouperish@gmail.com>
2022-04-01 15:33:30 +0200

    Small optimization

--- x.x_please-pass-the-coded-messages.py
+++ x.x_please-pass-the-coded-messages.py
@@ -1,29 +1,30 @@
 from itertools import combinations
 
 
-def helper(l, i):
-    rest = sum(l) % 3
-    for n in combinations(l, i):
-        if sum(n) % 3 == rest:
+def helper(l, r, n):
+    sl = [i < r for i in l]
+    for n in combinations(sl, n):
+        if sum(n) % 3 == r:
             l.remove(n[0])
             l.remove(n[1])
             return l
-    return helper(l, i+1)
+    return helper(l, r, n + 1)
 
 
 def solution(l):
     if l in [[0], [1], [2]]:
         return 0
     l = sorted(l, reverse=True)
-    rest = sum(l) % 3
-    while rest != 0:
-        if rest in l:
-            l.remove(rest)
+    r = sum(l) % 3
+    b = r
+    while b != 0:
+        if b in l:
+            l.remove(b)
             break
-        elif rest <= 8:
-            rest += 3
+        elif b <= 8:
+            b += 3
         else:
-            return helper(l, 2)
+            return helper(l, r, 2)
     return l
 
 
