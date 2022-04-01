74ba808752ac874f76c3aec9524d5674b1445fd0
Velcorn <iwishyouperish@gmail.com>
2022-04-02 00:23:22 +0200

    - Added dumb solution using superset - Added extra condition

--- x.x_please-pass-the-coded-messages.py
+++ x.x_please-pass-the-coded-messages.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from itertools import chain, combinations
 
 
 def solution(l):
@@ -17,18 +17,37 @@ def solution(l):
         elif b <= 8:
             b += 3
         else:
-            # Iterate over combinations of two elements of l in ascending order
+            # Iterate over subsets of two elements of l in ascending order
             for n in combinations(sorted(l), 2):
-                # If sum of elements has same r as l, remove them and return l
-                if sum(n) % 3 == r:
+                # If sum of elements is r, remove them and return l
+                if sum(n) == r:
                     l.remove(n[0])
                     l.remove(n[1])
                     b = 0
                     break
+            if b != 0:
+                b = 0
+                l = []
     # Return list in descending order
-    return sorted(l, reverse=True)
+    l = sorted(l, reverse=True)
+    return "".join(str(x) for x in l) if l else 0
+
+
+def solution_dumb(l):
+    # Dumb solution using superset
+    possible_numbers = chain.from_iterable(combinations(l, i) for i in range(len(l)+1))
+    biggest_number = 0
+    for pn in possible_numbers:
+        if not pn:
+            continue
+        if sum(pn) % 3 == 0:
+            number = int(''.join([str(n) for n in sorted(pn, reverse=True)]))
+            if number > biggest_number:
+                biggest_number = number
+    return biggest_number
 
 
 if __name__ == '__main__':
-    print(solution([3, 1, 4, 1]))
+    print(solution([5, 2]))
+    print(solution([3, 1, 4, 1, 1, 1]))
     print(solution([3, 1, 4, 1, 5, 9]))
