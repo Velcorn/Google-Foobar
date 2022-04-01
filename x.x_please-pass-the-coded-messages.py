d50ad1547a47429da50ad05c885f7632353c9728
velcorn <iwishyouperish@gmail.com>
2022-04-01 16:37:57 +0200

    Added comments

--- x.x_please-pass-the-coded-messages.py
+++ x.x_please-pass-the-coded-messages.py
@@ -1,46 +1,37 @@
 from itertools import combinations
 
 
-def helper(l, r, n):
-    # Get sublist of l only containing elements less than r
-    sl = [i < r for i in l]
-    # Test combinations of n elements of sublist
-    for n in combinations(sl, n):
-        # If sum of elements in combination equals r, remove elements from list and return list
-        if sum(n) % 3 == r:
-            l.remove(n[0])
-            l.remove(n[1])
-            return l
-    # Else recurse with n + 1
-    return helper(l, r, n + 1)
-
-
 def solution(l):
     # If sum of elements in list is less than 3, return empty 0
     if sum(l) < 3:
         return 0
     # Get rest when divided by 3
     r = b = sum(l) % 3
-    # If rest is 0, return l
-    if r == 0:
-        return l
-    # Sort list in descending order
-    l = sorted(l, reverse=True)
     # Loop while rest is not 0
     while b != 0:
         # If rest is 1, remove rest from list and return list
         if b in l:
             l.remove(b)
-            return l
+            break
         # If rest lte 8, increment b
         elif b <= 8:
             b += 3
-        # Else call helper to check multiple elements for removal
+        # Else determine two elements in list that sum to rest
         else:
-            return helper(l, r, 2)
+            # Get sublist of l only containing elements less than r
+            sl = [i < r for i in l]
+            # Test combinations of two elements of sublist
+            for n in combinations(sl, 2):
+                # If sum of elements in combination equals r, remove elements from list and return list
+                if sum(n) % 3 == r:
+                    l.remove(n[0])
+                    l.remove(n[1])
+                    b = 0
+                    break
+    # Return list in descending order
+    return sorted(l, reverse=True)
 
 
 if __name__ == '__main__':
-    print(solution([2, 4, 4, 1]))
     print(solution([3, 1, 4, 1]))
     print(solution([3, 1, 4, 1, 5, 9]))
