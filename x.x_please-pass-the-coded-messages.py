eae953950be66c60239fb7340677235ffebf2ddd
Velcorn <iwishyouperish@gmail.com>
2022-04-02 15:01:48 +0200

    If else > bad runtime xD

--- x.x_please-pass-the-coded-messages.py
+++ x.x_please-pass-the-coded-messages.py
@@ -1,53 +1,62 @@
-from itertools import chain, combinations
+def remove_elements(l, e1, e2):
+    l.remove(e1)
+    l.remove(e2)
+    return l
 
 
 def solution(l):
     # If sum of elements in list is less than 3, return empty 0
     if sum(l) < 3:
         return 0
-    # Get rest when divided by 3
-    r = b = sum(l) % 3
-    # Loop while rest is not 0
-    while b > 0:
-        # If rest is in l simply remove it
-        if b in l:
-            l.remove(b)
-            break
-        # If rest lte 8, increment
-        elif b <= 8:
-            b += 3
+    # Get rest and residual classes of mod 3
+    r = sum(l) % 3
+    rc = [r, r+3, r+6]
+    # If sum of elements in list is already divisible by 3, pass
+    if r == 0:
+        pass
+    # Elif any residual class is in l, simply remove it
+    elif any(r in l for r in rc):
+        for r in rc:
+            try:
+                l.remove(r)
+            except ValueError:
+                pass
+    # Else check residual classes and remove the smallest combination of two elements
+    else:
+        if r == 1:
+            if l.count(2) >= 2:
+                l = remove_elements(l, 2, 2)
+            elif 2 in l and 5 in l:
+                l = remove_elements(l, 2, 5)
+            elif l.count(5) >= 2:
+                l = remove_elements(l, 5, 5)
+            else:
+                l = remove_elements(l, 8, 8)
+        elif r == 2:
+            if l.count(1) >= 2:
+                l = remove_elements(l, 1, 1)
+            elif 1 in l and 7 in l:
+                l = remove_elements(l, 1, 7)
+            elif l.count(4) >= 2:
+                l = remove_elements(l, 4, 4)
+            else:
+                l = remove_elements(l, 7, 7)
+        elif r == 5:
+            l = remove_elements(l, 1, 4)
+        elif r == 7:
+            l = remove_elements(l, 2, 5)
         else:
-            # Iterate over subsets of two elements of l in ascending order
-            for n in combinations(sorted(l), 2):
-                # If sum of elements has same r, remove them
-                if sum(n) % 3 == r:
-                    l.remove(n[0])
-                    l.remove(n[1])
-                    b = 0
-                    break
-            # If removing two not possible, no solution exists
-            if b != 0:
-                b = 0
-                l = []
-    # Return list in descending order
+            if 1 in l and 7 in l:
+                l = remove_elements(l, 1, 7)
+            else:
+                l = remove_elements(l, 4, 4)
+    # Return list as string in descending order
     l = sorted(l, reverse=True)
-    return "".join(str(x) for x in l) if l else 0
-
-
-def solution_dumb(l):
-    # Dumb solution using superset
-    possible_numbers = chain.from_iterable(combinations(l, i) for i in range(len(l)+1))
-    biggest_number = 0
-    for pn in possible_numbers:
-        if not pn:
-            continue
-        if sum(pn) % 3 == 0:
-            number = int(''.join([str(n) for n in sorted(pn, reverse=True)]))
-            if number > biggest_number:
-                biggest_number = number
-    return biggest_number
+    return int("".join(str(x) for x in l)) if l else 0
 
 
 if __name__ == '__main__':
+    print(solution([5, 5]))
+    print(solution([9, 5, 2]))
     print(solution([3, 1, 4, 1]))
     print(solution([3, 1, 4, 1, 5, 9]))
