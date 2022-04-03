1b0dbcbacf0417810c5177142f88b41d3c7de084
Velcorn <iwishyouperish@gmail.com>
2022-04-03 20:02:35 +0200

    - Improved 3.2_bomb-baby.py

--- 3.2_bomb-baby.py
+++ 3.2_bomb-baby.py
@@ -1,29 +1,17 @@
 def solution(M, F):
     # Convert input to int
-    x, y = int(M), int(F)
+    x, y = max(int(M), int(F)), min(int(M), int(F))
     # Initiate counter
     rc = 0
-    # While not starting state
-    while x > 1 or y > 1:
-        # If either x or y is 1, just subtract value of other - 1
-        if x == 1:
-            rc += y-1
-            y -= y-1
-        elif y == 1:
-            rc += x-1
-            x -= x-1
-        # Elif either x or y is divisible by the other, it's impossible
-        elif x % y == 0 or y % x == 0:
-            return 'impossible'
-        # Else subtract the largest possible multiple of the smaller number from the larger number
-        elif x > y:
-            rc += x // y
-            x -= y * (x // y)
-        else:
-            rc += y // x
-            y -= x * (y // x)
-    # Return counter as string
-    return str(rc)
+    # While y > 1, subtract y time number y fits into x from x, then y is new x and x mod y is new y
+    while y > 1:
+        rc += x // y
+        x, y = y, x % y
+    # If y is 1, just need to subtract y from x x - 1 times
+    if y == 1:
+        return str(rc + x - 1)
+    else:
+        return 'impossible'
 
 
 if __name__ == '__main__':
