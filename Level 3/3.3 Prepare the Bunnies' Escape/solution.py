17e07289e5e5545fb05da8c95b60f4f6d8691905
Velcorn <iwishyouperish@gmail.com>
2022-04-04 13:09:07 +0200

    - Tried A* with Manhattan distance, still doesn't pass - Back to slightly improved/refactored BFS

--- Level 3/3.3 Prepare the Bunnies' Escape/solution.py
+++ Level 3/3.3 Prepare the Bunnies' Escape/solution.py
@@ -1,25 +1,28 @@
 def solution(map):
-    # Simple BFS with 1 optional removal of a wall
-    queue = [[True, [(0, 0)]]]
+    # BFS with 1 optional removal of a wall
+    door = (len(map) - 1, len(map[0]) - 1)
+    removal = True
+    queue = [[removal, [(0, 0)]]]
     visited = set()
-    # While queue, if current node is goal, return len of path, if node already visited, continue
-    # If not, add all possible moves to queue, and mark as visited; optionally remove a wall - single-use
+    # While queue, if current node is door, return len of path, if node already visited, continue
+    # If not, mark as visited, add all possible moves to queue and sort queue by md
+    # Optionally remove a wall - single-use
     while queue:
         rp = queue.pop(0)
         removal, path = rp[0], rp[1]
         x, y = path[-1]
-        if (x, y) == (len(map) - 1, len(map[0]) - 1):
+        if (x, y) == door:
             return len(path)
         if (x, y) in visited:
             continue
         visited.add((x, y))
         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < len(map) and 0 <= ny < len(map[0]):
+            if 0 <= nx <= door[0] and 0 <= ny <= door[1]:
                 if map[nx][ny] == 0:
-                    queue.append([removal, path + [(nx, ny)]])
+                    queue += [[removal, path + [(nx, ny)]]]
                 elif removal:
-                    queue.append([False, path + [(nx, ny)]])
+                    queue += [[False, path + [(nx, ny)]]]
 
 
 if __name__ == '__main__':
