8f033cc7eedb51fb8e513217bb164b42d44777e1
Velcorn <iwishyouperish@gmail.com>
2022-04-05 13:10:16 +0200

    Finished hey-i-already-did-that.py

--- Level 1/1.x Hey, I Already Did That!/hey-i-already-did-that.py
+++ Level 1/1.x Hey, I Already Did That!/hey-i-already-did-that.py
@@ -1,18 +1,35 @@
+def int2base(n, b):
+    # Convert an integer n to base b
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(str(int(n % b)))
+        n /= b
+    return ''.join(digits[::-1])
+
+
 def solution(n, b):
+    # Initialize ids generated so far and keep track of them in a set
     ids = [n]
+    unique = set(n)
     while True:
+        # Split n into digits, sort them and create x and y from the sorted digits
         digits = sorted([i for i in str(n)])
-        print(digits)
         x = ''.join(digits)
         y = ''.join(digits[::-1])
-        n = str(int(y, b) - int(x, b)).zfill(len(n))
-        print(n)
+        # Subtract x from y and convert the result to base b
+        n = int2base(int(y, b) - int(x, b), b).zfill(len(n))
+        # If n is '0', return 1
+        # Elif check if n has been seen before, then return cycle length
+        # Else append/add n to the list/set and continue
         if n == '0':
             return 1
-        if n in ids:
+        elif n in unique:
             return len(ids) - ids.index(n)
         else:
             ids.append(n)
+            unique.add(n)
 
 
 if __name__ == '__main__':
