a9f8c20373d9238b9bdd8085dfc2759429dc8a93
velcorn <iwishyouperish@gmail.com>
2023-02-18 01:09:26 +0100

    Why is ChatGPT so good at this?!

--- Level 4/4.x Free the Bunny Workers/solution.py
+++ Level 4/4.x Free the Bunny Workers/solution.py
@@ -2,11 +2,16 @@ from itertools import combinations
 
 
 def solution(num_buns, num_required):
+    # Calculate the number of keys required per bunny
     keys_per_bunny = num_buns - num_required + 1
+    # Initialize empty list of keys
     bunnies = [[] for i in range(num_buns)]
+    # Iterate over the enumeration of combinations of keys with range(num_buns) and keys_per_bunny
     for i, combination in enumerate(combinations(range(num_buns), keys_per_bunny)):
+        # Iterate over the combination
         for bunny in combination:
-            bunnies[bunny].append(i)
+            # Append the current combination to the list of keys for the current bunny
+            bunnies[bunny] += [i]
     return bunnies
 
 
