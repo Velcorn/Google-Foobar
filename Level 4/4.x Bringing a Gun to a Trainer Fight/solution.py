2406414b223af1e27f1935381d369474f4d8c3a2
velcorn <iwishyouperish@gmail.com>
2023-02-21 00:18:33 +0100

    Legit fuck this problem

---
+++ Level 4/4.x Bringing a Gun to a Trainer Fight/solution.py
@@ -0,0 +1,7 @@
+def solution(dimensions, your_position, trainer_position, distance):
+    pass
+
+
+if __name__ == "__main__":
+    print(solution([3, 2], [1, 1], [2, 1], 4))
+    print(solution([300, 275], [150, 150], [185, 100], 500))
